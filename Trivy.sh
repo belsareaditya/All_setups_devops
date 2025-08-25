bb52ca8502887f634abd1f215907709de8ca5694
Aditya Belsare <71834027+belsareaditya@users.noreply.github.com>
2025-08-25 19:46:50 +0530

    Update Trivy.sh

--- Trivy.sh
+++ Trivy.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# Install prerequisites
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
-sudo apt-get install -y wget apt-transport-https gnupg lsb-release
-
-# Add Aqua Security’s Trivy repo key
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key \
-  | gpg --dearmor \
-  | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
-
-# Add Trivy apt repo
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" \
-  | sudo tee /etc/apt/sources.list.d/trivy.list
-
-# Update repos and install Trivy
-sudo apt-get update
-sudo apt-get install -y trivy
+sudo apt-get install trivy
 
 # Verify
 trivy --version
