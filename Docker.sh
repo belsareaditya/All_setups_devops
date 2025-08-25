2c57943387c643ec759c5b64664df62b626b7603
Aditya Belsare <71834027+belsareaditya@users.noreply.github.com>
2025-08-25 19:31:12 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -4,18 +4,27 @@
 apt-get update
 apt-get upgrade -y
 
-# Install Docker dependencies
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+# Install necessary dependencies
+sudo apt-get install -y ca-certificates curl
 
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Create directory for Docker GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
 
-# Add Docker repository
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Download Docker's GPG key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
-# Update package information and install Docker
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+# Ensure proper permissions for the key
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker repository to Apt sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package manager repositories
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
 
 # Start Docker service
 systemctl start docker
